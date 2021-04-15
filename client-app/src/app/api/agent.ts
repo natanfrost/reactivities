0a5ddc417c1cbd09dad1a1643741ea921452c132
Nataniel <nataniel.r.v@gmail.com>
2021-04-15 12:03:34 -0300

    MobX added to state control

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -23,8 +23,8 @@ const responseBody = <T> (response: AxiosResponse<T>) => response.data;
 
 const requests = {
     get: <T> (url: string) => axios.get<T>(url).then(responseBody),
-    post: <T> (url: string, body: {}) => axios.post<T>(url).then(responseBody),
-    put: <T> (url: string, body: {}) => axios.put<T>(url).then(responseBody),
+    post: <T> (url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
+    put: <T> (url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
     del: <T> (url: string) => axios.delete<T>(url).then(responseBody)
 }
 
@@ -32,8 +32,8 @@ const Activities = {
     list: () => requests.get<Activity[]>('/activities'),
     details:(id: string) => requests.get<Activity>(`/activities/${id}`),
     create: (activity: Activity) => requests.post('/activities', activity),
-    update: (activity: Activity) => axios.put(`/activities/${activity.id}`, activity),
-    delete: (id: string) => axios.delete(`/activities/${id}`)
+    update: (activity: Activity) => requests.put(`/activities/${activity.id}`, activity),
+    delete: (id: string) => requests.del(`/activities/${id}`)
 }
 
 const agent = {
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,56 +1,32 @@
+import { observer } from 'mobx-react-lite';
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
-import { Activity } from '../../../app/models/activity';
+import { useStore } from '../../../app/stores/store';
 import ActivityDetails from '../details/ActivityDetails';
 import ActivityForm from '../form/ActivityForm';
 import ActivityList from './ActivityList';
 
-interface Props {
-    activities: Activity[];
-    selectedActivity: Activity | undefined;
-    selectActivity: (id: string) => void;
-    cancelSelectetActivity: () => void;
-    editMode: boolean;
-    openForm: (id: string) => void;
-    closeForm: () => void;
-    createEdit: (activity: Activity) => void;
-    deleteActivity: (id: string) => void;
-    submitting: boolean;
-}
+export default observer(function ActivityDashboard() {
 
-export default function ActivityDashboard({activities, selectActivity, selectedActivity, cancelSelectetActivity, 
-    openForm, closeForm, editMode, createEdit, deleteActivity, submitting} : Props) {
+    const {activityStore} = useStore();
+    const {selectedActivity, editMode} = activityStore;
     return (
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList 
-                    activities={activities} 
-                    selectActivity={selectActivity} 
-                    deleteActivity={deleteActivity}
-                    submitting={submitting}
-                />
+                <ActivityList />
             </Grid.Column>
             <Grid.Column width='6'>
                 {                    
                     selectedActivity && !editMode &&
-                    <ActivityDetails 
-                        activity={selectedActivity} 
-                        cancelSelectActivity={cancelSelectetActivity}
-                        openForm={openForm}
-                    />
+                    <ActivityDetails />
                 }
 
                 {
                     editMode && 
-                    <ActivityForm 
-                        closeForm={closeForm} 
-                        activity={selectedActivity} 
-                        createEdit={createEdit}
-                        submitting={submitting}
-                    />
+                    <ActivityForm />
                 }
                 
             </Grid.Column>
         </Grid>
     )
-}
+})
